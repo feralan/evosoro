1160e46dbdb52694fb4aa186dbb900be5503bec5
Francesco Iberite <thed4rkside@gmail.com>
2017-06-04 19:52:43 +0200

    Calcolo fitness

--- evosoro/tools/simulatedFitness.py
+++ evosoro/tools/simulatedFitness.py
@@ -4,26 +4,26 @@ def calcInterDistance(weightMatrix, additionalData):
     calculatedValues = [];
     res = 0;
 
-    for index in range(1, len(additionalData.pressures)):
-        calculatedValues[index] = np.sum(np.multiply(additionalData.pressures[index], weightMatrix))
+    for index in range(0, len(additionalData.pressures)):
+        calculatedValues.append(np.sum(np.multiply(additionalData.pressures[index], weightMatrix)))
 
-    for indIndex in range(1, len(additionalData.pressures)):
-        for index in range(1, len(additionalData.pressures)):
+    for indIndex in range(0, len(additionalData.pressures)):
+        for index in range(0, len(additionalData.pressures)):
             if additionalData.labels[indIndex] != additionalData.labels[index]:
                 res += np.abs(calculatedValues[indIndex] - calculatedValues[index])
-
+    
     return res
 
 def calcIntraDistance(weightMatrix, additionalData):
     calculatedValues = [];
     res = 0;
 
-    for index in range(1, len(additionalData.pressures)):
-        calculatedValues[index] = np.sum(np.multiply(additionalData.pressures[index], weightMatrix))
+    for index in range(0, len(additionalData.pressures)):
+        calculatedValues.append(np.sum(np.multiply(additionalData.pressures[index], weightMatrix)))
 
-    for indIndex in range(1, len(additionalData.pressures)):
-        for index in range(1, len(additionalData.pressures)):
+    for indIndex in range(0, len(additionalData.pressures)):
+        for index in range(0, len(additionalData.pressures)):
             if additionalData.labels[indIndex] == additionalData.labels[index]:
                 res += np.abs(calculatedValues[indIndex] - calculatedValues[index])
-
+    
     return res
